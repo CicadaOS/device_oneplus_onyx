bac1319b6ceac18ac2a12d99358e6c16424142e6
Bingco <ittat@live.com>
2020-06-29 09:35:03 +0800

    Update device.mk

--- device.mk
+++ device.mk
@@ -16,9 +16,6 @@
 
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 
-# Overlays
-DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay
 
 PRODUCT_ENFORCE_RRO_TARGETS := \
     framework-res
